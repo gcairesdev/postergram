9b734547667742c48be558daadb028dad0328c62
Guilherme Caires <pessoalgcaires@gmail.com>
2022-02-08 18:52:48 -0300

    build: added node-cron

--- index.js
+++ index.js
@@ -2,54 +2,57 @@ const express = require('express');
 const app = express();
 const Instagram = require('instagram-web-api');
 const FileCookieStore = require('tough-cookie-filestore2');
+const cron = require('node-cron');
 require('dotenv').config()
 
 const port = process.env.PORT || 4000
 
-const cookieStore = new FileCookieStore('./cookies.json');
+cron.schedule('0 19 * * 4', async () => {
+    const cookieStore = new FileCookieStore('./cookies.json');
 
-const client = new Instagram(
-    {
-        username: process.env.INSTAGRAM_USERNAME,
-        password: process.env.INSTAGRAM_PASSWORD,
-        cookieStore,
-    },
-    {
-        language: 'en-US',
-    }
-);
+    const client = new Instagram(
+        {
+            username: process.env.INSTAGRAM_USERNAME,
+            password: process.env.INSTAGRAM_PASSWORD,
+            cookieStore,
+        },
+        {
+            language: 'en-US',
+        }
+    );
 
-const login = async () => {
-    console.log('Loggin in...');
+    const login = async () => {
+        console.log('Loggin in...');
 
-    await client.login().then(() => {
-        console.log('Login successful!');
-        post();
-    }).catch((err) => {
-        console.log('Login failed!');
-        console.log(err);
-    });
-};
-
-const post = async () => {
-    await client.uploadPhoto({
-        photo: './img/1.jpg',
-        caption: 'Legenda',
-        post: 'feed'
-    }).then(async (res) => {
-        const media = res.media;
-
-        console.log(`https://instagram.com/p/${media.code}`);
-
-        await client.addComment({
-            mediaId: media.id,
-            text: "Comentário"
+        await client.login().then(() => {
+            console.log('Login successful!');
+            post();
+        }).catch((err) => {
+            console.log('Login failed!');
+            console.log(err);
         });
-    });
-};
+    };
+
+    const post = async () => {
+        await client.uploadPhoto({
+            photo: './img/1.jpg',
+            caption: 'Legenda',
+            post: 'feed'
+        }).then(async (res) => {
+            const media = res.media;
+
+            console.log(`https://instagram.com/p/${media.code}`);
+
+            await client.addComment({
+                mediaId: media.id,
+                text: "Comentário"
+            });
+        });
+    };
+
+    login();
+});
 
 app.listen(port, () => {
     console.log(`Running on port ${port}...`)
 });
-
-login();
